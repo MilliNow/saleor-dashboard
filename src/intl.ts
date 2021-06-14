45dab48621cf3c8c88ac0aec7a5c9161f6b7ea69
Ahmed Jazzar <11036472+ahmedaljazzar@users.noreply.github.com>
2021-06-13 21:33:52 -0700

    Edit somethingWentWrong message

--- src/intl.ts
+++ src/intl.ts
@@ -81,7 +81,7 @@ export const commonMessages = defineMessages({
     defaultMessage: "Your session has expired. Please log in again to continue."
   },
   somethingWentWrong: {
-    defaultMessage: "Saleor ran into an unexpected problem"
+    defaultMessage: "We ran into an unexpected problem"
   },
   startDate: {
     defaultMessage: "Start Date"
